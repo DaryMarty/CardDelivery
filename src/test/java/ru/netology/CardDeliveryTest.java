fbdabc2340249610bbb432bce293764547e94344
Darya Martemyanova <mdy.89@mail.ru>
2022-02-18 16:06:34 +0300

    Add headless, created Date

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -10,34 +10,38 @@ import org.openqa.selenium.Keys;
 import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.zip.DataFormatException;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
     public static String deleteString = Keys.chord(Keys.CONTROL, "a") + Keys.DELETE;
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    String planningDate = generateDate(4);
 
     @Test
     public void shouldOrderCreated() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_YEAR, 3);
-        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
-        String date = format.format(calendar.getTime());
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Казань");
         $("[placeholder='Дата встречи']").sendKeys(deleteString);
-        $("[placeholder='Дата встречи']").setValue(date);
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79991892310");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
         $("[data-test-id='notification']").shouldHave(Condition.visible, Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(text("Встреча успешно забронирована на " + planningDate));
 
     }
 }
